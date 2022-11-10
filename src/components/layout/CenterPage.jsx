fcfc5294fccd758fdcacde00d81763bbf8d29aee
Franzley <fbonhomme9721@gmail.com>
2022-11-09 21:00:28 -0500

    Change layout for each web path

--- src/components/layout/CenterPage.jsx
+++ src/components/layout/CenterPage.jsx
@@ -2,18 +2,9 @@ import React from "react";
 import { Box } from "@mui/material";
 
 const CenterPage = () => {
-  const testArr = [];
-
-  for (let i = 0; i < 50; i++) testArr.push(i);
-
   return (
     <Box sx={{ color: "white", borderRight: 1, borderLeft: 1 }}>
       CenterPage
-      {testArr.map(() => (
-        <Box sx={{ color: "white", borderRight: 1, borderLeft: 1 }}>
-          CenterPage
-        </Box>
-      ))}
     </Box>
   );
 };
